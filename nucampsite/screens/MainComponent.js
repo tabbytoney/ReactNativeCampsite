0a9ce219fb293fa53d1d2c2982510633958246f8
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-18 11:20:50 -0500

    Add about and contact screen

--- nucampsite/screens/MainComponent.js
+++ nucampsite/screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -15,7 +17,7 @@ const screenOptions = {
   headerTintColor: '#fff',
 };
 
-// for drawer navigation
+// for stack navigation
 const HomeNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -50,6 +52,30 @@ const DirectoryNavigator = () => {
   );
 };
 
+// for stack navigation
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name='About' component={AboutScreen} />
+    </Stack.Navigator>
+  );
+};
+
+// for stack navigation
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Contact'
+        component={ContactScreen}
+        options={{ title: 'Contact Us' }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const Main = () => {
   return (
     <View
@@ -71,6 +97,14 @@ const Main = () => {
           name='Directory'
           component={DirectoryNavigator}
           options={{ title: 'Directory' }}
+        />
+
+        <Drawer.Screen name='About' component={AboutNavigator} />
+
+        <Drawer.Screen
+          name='Contact'
+          component={ContactNavigator}
+          options={{ title: 'Contact Us' }}
         />
       </Drawer.Navigator>
     </View>
