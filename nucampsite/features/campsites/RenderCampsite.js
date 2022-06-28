782148dc94775c64b04f6d58dc73b419f88f1b5d
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-28 16:50:40 -0500

    Make favorites toggle on AND off

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -21,11 +21,7 @@ const RenderCampsite = (props) => {
             color='#f50'
             raised
             reverse
-            onPress={() =>
-              props.isFavorite
-                ? console.log('Already set as a favorite')
-                : props.markFavorite()
-            }
+            onPress={() => props.markFavorite()}
           />
           <Icon
             name='pencil'
