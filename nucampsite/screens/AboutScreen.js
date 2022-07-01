17b15103d3154eb263d48767a19fa052807476ab
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-30 22:08:58 -0500

    Added animations for each screen

--- nucampsite/screens/AboutScreen.js
+++ nucampsite/screens/AboutScreen.js
@@ -1,4 +1,5 @@
 import { ScrollView } from 'react-native-gesture-handler';
+import * as Animatable from 'react-native-animatable';
 import { Avatar, Card, ListItem, Text } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -41,31 +42,38 @@ const AboutScreen = () => {
   if (partners.errMess) {
     return (
       <ScrollView>
-        <Mission />
-        <Card>
-          <Card.Title>Community Partners</Card.Title>
-          <Card.Divider />
-          <Text>{partners.errMess}</Text>
-        </Card>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Mission />
+          <Card>
+            <Card.Title>Community Partners</Card.Title>
+            <Card.Divider />
+            <Text>{partners.errMess}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
   return (
     <ScrollView>
-      <Mission />
-      <Card>
-        <Card.Title>Community Partners</Card.Title>
-        <Card.Divider />
-        {partners.partnersArray.map((partner) => (
-          <ListItem key={partner.id}>
-            <Avatar rounded source={{ uri: baseUrl + partner.image }}></Avatar>
-            <ListItem.Content>
-              <ListItem.Title>{partner.name}</ListItem.Title>
-              <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </Card>
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          {partners.partnersArray.map((partner) => (
+            <ListItem key={partner.id}>
+              <Avatar
+                rounded
+                source={{ uri: baseUrl + partner.image }}
+              ></Avatar>
+              <ListItem.Content>
+                <ListItem.Title>{partner.name}</ListItem.Title>
+                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </Card>
+      </Animatable.View>
     </ScrollView>
   );
 };
