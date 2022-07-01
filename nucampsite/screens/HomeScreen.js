acd395551839ff009e4be74c3d330bb50c0a4a6c
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-06-30 21:51:36 -0500

    Home screen card animation on load

--- nucampsite/screens/HomeScreen.js
+++ nucampsite/screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import { Text, View, ScrollView } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Text, View, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -10,7 +11,6 @@ const FeaturedItem = (props) => {
   if (props.isLoading) {
     return <Loading />;
   }
-
   if (props.errMess) {
     return (
       <View>
@@ -18,7 +18,6 @@ const FeaturedItem = (props) => {
       </View>
     );
   }
-
   if (item) {
     return (
       <Card containerStyle={{ padding: 0 }}>
@@ -46,6 +45,12 @@ const HomeScreen = () => {
   const campsites = useSelector((state) => state.campsites);
   const promotions = useSelector((state) => state.promotions);
   const partners = useSelector((state) => state.partners);
+  const scaleValue = useRef(new Animated.Value(0)).current;
+  const scaleAnimation = Animated.timing(scaleValue, {
+    toValue: 1,
+    duration: 1500,
+    useNativeDriver: true,
+  });
 
   const featCampsite = campsites.campsitesArray.find((item) => item.featured);
   const featPromotion = promotions.promotionsArray.find(
@@ -53,8 +58,12 @@ const HomeScreen = () => {
   );
   const featPartner = partners.partnersArray.find((item) => item.featured);
 
+  useEffect(() => {
+    scaleAnimation.start();
+  }, []);
+
   return (
-    <ScrollView>
+    <Animated.ScrollView style={{ transform: [{ scale: scaleValue }] }}>
       <FeaturedItem
         item={featCampsite}
         isLoading={campsites.isLoading}
@@ -70,7 +79,7 @@ const HomeScreen = () => {
         isLoading={partners.isLoading}
         errMess={partners.errMess}
       />
-    </ScrollView>
+    </Animated.ScrollView>
   );
 };
 
