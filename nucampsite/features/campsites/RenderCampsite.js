c43b63b43e20d937eeff28c7019c0042266321bf
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-07-06 19:03:21 -0500

    Add social sharing abilities

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+  Alert,
+  Share,
+} from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
@@ -50,6 +57,19 @@ const RenderCampsite = (props) => {
     },
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title,
+        message: `${title}: ${message} ${url}`,
+        url,
+      },
+      {
+        dialogTitle: 'Share ' + title,
+      }
+    );
+  };
+
   if (campsite) {
     return (
       <Animatable.View
@@ -86,6 +106,20 @@ const RenderCampsite = (props) => {
               raised
               reverse
               onPress={props.onShowModal}
+            />
+            <Icon
+              name='share'
+              type='font-awesome'
+              color='#5637DD'
+              raised
+              reverse
+              onPress={() =>
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
